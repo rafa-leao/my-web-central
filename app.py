53317bdfd14a4304b4c015c5540176076f2756c7
rafa-leao <rleao53@gmail.com>
2019-02-23 18:22:32 -0300

    show the bus arrival

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template
 
 import api.weather
+import api.bus_traffic
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
 
-    weather = api.weather.WeatherAPI()
+    weather_info = api.weather.WeatherAPI()
+    temperature_now = weather_info.temperature_now()
 
-    temperature_now = weather.temperature_now()
+    bus_info = api.bus_traffic.BusAPI()
+    bus_arrival = bus_info.arrival_forecast(650005666, 932)
 
-    return render_template('index.html', temperature_now=temperature_now )
+    return render_template('index.html', temperature_now=temperature_now, bus_arrival=bus_arrival)
 
 
 app.run(debug=True)
