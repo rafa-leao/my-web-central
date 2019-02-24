65efee45e43225c9ce4d6d14b6919d31b93636a0
rafa-leao <rleao53@gmail.com>
2019-02-23 22:02:37 -0300

    now is possible to show the temperature over 5 hours

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 
+import time
+
 import api.weather
 import api.bus_traffic
 
@@ -15,7 +17,23 @@ def index():
     bus_info = api.bus_traffic.BusAPI()
     bus_arrival = bus_info.arrival_forecast(650005666, 932)
 
-    return render_template('index.html', temperature_now=temperature_now, bus_arrival=bus_arrival)
+    time_now = time.localtime()[3]
+
+    one_hour_later = [time_now + 1,
+                      time_now + 2,
+                      time_now + 3,
+                      time_now + 4]
+
+    temperature_to_one_hour_later = [weather_info.temperature(time_now+1),
+                      weather_info.temperature(time_now+2),
+                      weather_info.temperature(time_now+3),
+                      weather_info.temperature(time_now+4)]
+
+    return render_template('index.html',
+                           temperature_now=temperature_now,
+                           bus_arrival=bus_arrival,
+                           one_hour_later=one_hour_later,
+                           temperature_to_one_hour_later=temperature_to_one_hour_later)
 
 
 app.run(debug=True)
